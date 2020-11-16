8a80d0627ed84d1d35c1a3c3d13282c2f84c860c
Benedikt Radtke <benediktradtke@gmail.com>
2020-11-16 13:47:13 +0100

    Fix linefeeds

--- service/WASP/Controllers/AddAttackController.cs
+++ service/WASP/Controllers/AddAttackController.cs
@@ -5,8 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WASP.Storage;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace WASP.Controllers
 {
     [Route("api/[controller]")]
